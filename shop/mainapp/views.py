a444fbc895c7e7b9acb62bd4916b428dc4b3f9d6
egor <egor555550@mail.ru>
2021-09-01 18:12:23 +0500

    lesson 20

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
 
 # Create your views here.
 
-def test_view(requests):
-    categories = Category.objects.get_categories_for_left_slider()
-    return render(requests, 'base.html', {'categories': categories})
+class BaseView(View):
+    def get(self, requests, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_slider()
+        return render(requests, 'base.html', {'categories': categories})
+
+# def test_view(requests):
+#     categories = Category.objects.get_categories_for_left_slider()
+#     return render(requests, 'base.html', {'categories': categories})
 
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView):
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone,
@@ -27,7 +33,7 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
     model = Category
     queryset = Category.objects.all()
 
