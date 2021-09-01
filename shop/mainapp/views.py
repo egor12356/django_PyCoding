4b1202a36145eff798281f15d4ceef4b5f068af5
egor <egor555550@mail.ru>
2021-09-01 22:41:29 +0500

    lesson 22

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
-from .models import Notebook, Smartphone, Category, LatestProducts
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
 from .mixins import CategoryDetailMixin
 
 # Create your views here.
@@ -48,3 +48,17 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class CartView(View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories,
+        }
+
+        return render(request, 'cart.html', context)
